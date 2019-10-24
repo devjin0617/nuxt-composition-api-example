18f8d9e0df1c2f053e607e5e13710283c6f121e5
jin-mealant <jin@mealant.com>
2019-10-24 11:57:09 +0900

    Add composition-api sample code

--- pages/index.vue
+++ pages/index.vue
@@ -2,34 +2,41 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        nuxt-composition-api-example
-      </h1>
-      <h2 class="subtitle">
-        My finest Nuxt.js project
-      </h2>
+      <h1 class="title">{{ state.value }}</h1>
+      <h2 class="subtitle">My finest Nuxt.js project</h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
+          >Documentation</a
+        >
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
           class="button--grey"
+          >GitHub</a
         >
-          GitHub
-        </a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { onMounted, reactive } from '@vue/composition-api'
 import Logo from '~/components/Logo.vue'
 
 export default {
   components: {
     Logo
+  },
+  setup() {
+    const state = reactive({
+      value: ''
+    })
+    onMounted(() => {
+      state.value = 'hello composotion api!'
+    })
+    return {
+      state
+    }
   }
 }
 </script>
